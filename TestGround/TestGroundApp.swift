d21c31fed4d7e4720ba84825b1245a27217377c4
Matt Maher <matt@maher-family.net>
2023-06-21 19:06:01 -0700

    Initial Commit

---
+++ TestGround/TestGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGroundApp.swift
+//  TestGround
+//
+//  Created by Matt Maher on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
