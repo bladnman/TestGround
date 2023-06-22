145f3b5e6a71ba68253cae7598cc5f1a24d2e995
Matt Maher <matt.maher@sony.com>
2023-06-22 16:42:10 -0700

    WIP

--- TestGround/States/States.swift
+++ TestGround/States/States.swift
@@ -8,40 +8,74 @@
 import SwiftUI
 
 struct States: View {
+    @State var name = "Bob"
+    var age = 25
     var body: some View {
-        
-            Parent()
-        
+        VStack {
+            Text("Name: \(name) (\(age))")
+                .font(.headline)
+            Parent(name: name, age: age)
+        }
     }
+}
+
+struct Parent: View {
+    @State var name: String
+    var age = 15
+    var body: some View {
+        VStack {
+            Text("Name: \(name) (\(age))")
+                .font(.headline)
+
+            HStack {
+                ForEach(PersonName.allCases, id: \.self) { tName in
+                    Button(action: {
+                        name = tName.rawValue
+                    }) {
+                        Text(tName.rawValue)
+                    }
+                }
+            }
+
+            Child(name: $name)
+        }
+        .padding(30)
+        .background(.green)
+    }
+}
+
+struct Child: View {
+    @Binding var name: String
+    var body: some View {
+        VStack {
+            Text("Name: \(name)")
+                .font(.headline)
+                .padding(.bottom)
+            HStack {
+                Button("Bob") {
+                    name = "Bob"
+                }
+                Button("Jim") {
+                    name = "Jim"
+                }
+                Button("Sally") {
+                    name = "Sally"
+                }
+            }
+        }
+        .padding(30)
+        .background(.yellow)
+    }
+}
+
+enum PersonName: String, CaseIterable {
+    case bob = "Bob"
+    case jim = "Jim"
+    case sally = "Sally"
 }
 
 struct States_Previews: PreviewProvider {
     static var previews: some View {
         States()
-    }
-}
-
-struct Parent: View {
-    
-    var body: some View {
-        VStack {
-            Text("Parent")
-                .font(.headline)
-            Child()
-        }
-        .background(.green)
-        .padding(30)
-    }
-}
-struct Child: View {
-
-    var body: some View {
-        
-        VStack {
-            Text("Child")
-                .font(.headline)
-        }
-        .background(.yellow)
-        .padding(30)
     }
 }
---
+++ TestGround/TestGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGroundApp.swift
+//  TestGround
+//
+//  Created by Matt Maher on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            States()
+        }
+    }
+}
